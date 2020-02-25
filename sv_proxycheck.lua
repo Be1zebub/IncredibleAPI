c4e8df14225f65595619683790b1e6f9d47c0c26
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-02-25 16:50:42 +0700

    Update sv_proxycheck.lua

--- sv_proxycheck.lua
+++ sv_proxycheck.lua
@@ -5,39 +5,37 @@
         EMail: beelzebub@incredible-gmod.ru
         Discord: discord.incredible-gmod.ru
 --——————————————————————————————————————————————]]--
-Incredible_API = Incredible_API or {}
-Incredible_API.Cache = Incredible_API.Cache or {}
-
-Incredible_API.Cache.Proxy = Incredible_API.Cache.Proxy or {}
+IncredibleAPI = IncredibleAPI or {}
+IncredibleAPI.Cache = IncredibleAPI.Cache or {}
+IncredibleAPI.Cache.Proxy = IncredibleAPI.Cache.Proxy or {}
 
 local http_Fetch = http.Fetch
 local Is_Valid = IsValid
 
-Incredible_API.ProxyByIP = function(ip, callback)
-	if not ip then return end
+IncredibleAPI.ProxyByIP = function(ip, callback)
+    if not ip then return end
 
-	if Incredible_API.Cache.Proxy[ip] then
-		if callback then
-			callback(Incredible_API.Cache.Proxy[ip])
-		end
-		return
-	end
+    if IncredibleAPI.Cache.Proxy[ip] then
+        if callback then
+            callback(IncredibleAPI.Cache.Proxy[ip])
+        end
 
-	http_Fetch("https://blackbox.ipinfo.app/lookup/"..ip, function(body)
-		if not body or body == "" then return end
+        return
+    end
 
-		Incredible_API.Cache.Proxy[ip] = body == "Y" --(body == "Y" and true or body == "N" and false)
+    http_Fetch("https://blackbox.ipinfo.app/lookup/" .. ip, function(body)
+        if not body or body == "" then return end
+        IncredibleAPI.Cache.Proxy[ip] = body == "Y" --(body == "Y" and true or body == "N" and false)
 
-		if callback then
-			callback(Incredible_API.Cache.Proxy[ip])
-		end
-	end)
+        if callback then
+            callback(IncredibleAPI.Cache.Proxy[ip])
+        end
+    end)
 end
 
-Incredible_API.Proxy = function(ply, callback)
-	if not Is_Valid(ply) then return end
-
-	Incredible_API.ProxyByIP(ply:IPAddress(), callback)
+IncredibleAPI.Proxy = function(ply, callback)
+    if not Is_Valid(ply) then return end
+    IncredibleAPI.ProxyByIP(ply:IPAddress(), callback)
 end
 
 
@@ -45,7 +43,7 @@ end
 	Usage:
 
 	hook.Add("PlayerAuthed", "AntiProxy", function(ply)
-		Incredible_API.Proxy(ply, function(isproxy)
+		IncredibleAPI.Proxy(ply, function(isproxy)
 			if isproxy then
 				ply:Kick("Я ТЕБЯ ЗАПРЕЩАЮ!")
 			end
