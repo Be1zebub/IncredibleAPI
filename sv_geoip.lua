17f3160bba7d5700f96b5926b64f08bdb304b404
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-02-26 22:44:46 +0700

    Create sv_geoip.lua

---
+++ sv_geoip.lua
@@ -0,0 +1,53 @@
+--[[——————————————————————————————————————————————--
+             Developer: [INC]Be1zebub
+
+         Website: incredible-gmod.ru/owner
+        EMail: beelzebub@incredible-gmod.ru
+        Discord: discord.incredible-gmod.ru
+--——————————————————————————————————————————————]]--
+
+IncredibleAPI = IncredibleAPI or {}
+IncredibleAPI.Cache = IncredibleAPI.Cache or {}
+IncredibleAPI.Cache.GeoIP = IncredibleAPI.Cache.GeoIP or {}
+
+
+local http_Fetch = http.Fetch
+local util_JSONToTable = util.JSONToTable
+local Is_Valid = IsValid
+
+--- http://ip-api.com/json/12.34.5.6?fields=countryCode  (Another GeoAPI)
+
+IncredibleAPI.GeoIPByIP = function(ip, callback)
+	if not ip then return end
+
+	if IncredibleAPI.Cache.GeoIPip] then
+		if callback then
+			callback(IncredibleAPI.Cache.GeoIPip])
+		end
+		return
+	end
+	
+	http_Fetch("https://freegeoip.app/json/"..ip, function(body)
+		if not body or body == "" then return end
+		local tbl = util_JSONToTable(body)
+		if not tbl or not tbl["country_code"] then return end
+
+		IncredibleAPI.Cache.GeoIPip] = tbl["country_code"]
+	end)
+end
+
+IncredibleAPI.GeoIP = function(ply, callback)
+	if not Is_Valid(ply) then return end
+
+	IncredibleAPI.GeoIPByIP(ply:IPAddress(), callback)
+end
+
+--[[
+  Usage:
+  
+   hook.Add("PlayerAuthed", "API_GeoIP_Test", function(ply)
+    IncredibleAPI.GeoIP(ply, function(country_code)
+      print(ply:Nick(), "ip has:", country_code, "country code")
+    end
+   end)
+]]--
