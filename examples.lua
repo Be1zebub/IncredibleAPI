88f29e10a9a84aaaabbd76166d3ba2e29b3c8768
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-06-12 21:31:40 +0700

    Update examples.lua

--- examples.lua
+++ examples.lua
@@ -1,16 +1,35 @@
 -- Usage examples:
 
-IncredibleAPI:Call("SteamPlaytime", player.GetHumans()[1]:SteamID64(), "YorSteamAPIKeyHere", function(seconds) -- Get gmod playtime
+IncredibleAPI:Call("WebMaterials", "http://incredible-gmod.ru/assets/other/beelze_pixel.png", function(webMat) -- Get web material
+	print(webMat)
+	if CLIENT then
+		hook.Add("HUDPaint", "DrawWebMatExample", function()
+			surface.SetDrawColor(255, 255, 255, 255)
+			surface.SetMaterial(webMat)
+			surface.DrawTexturedRect(ScrW()*0.5 - 128, ScrH()*0.5 - 120.5, 256, 241)
+		end)
+	end
+end)
+
+local ply = player.GetHumans()[1]
+
+IncredibleAPI:Call("GeoIP", ply, function(country_code) -- Get country code
+	print(country_code)
+end)
+
+IncredibleAPI:Call("Proxy", ply, function(is_use_proxy) -- Anti-proxy API
+	ply:Kick("Proxy is not allowed!")
+end)
+
+IncredibleAPI:Call("SteamPlaytime", ply, "!!! Your SteamAPI-Key Here !!!", function(seconds) -- Get gmod playtime
 	print(seconds/60)
 end)
 
-IncredibleAPI:Call("WebMaterials", "http://incredible-gmod.ru/assets/other/beelze_pixel.png", function(webMat) -- Get web material
-  print(webMat)
-  if CLIENT then
-    hook.Add("HUDPaint", "DrawWebMatExample", function()
-        surface.SetDrawColor(255, 255, 255, 255)
-        surface.SetMaterial(webMat)
-        surface.DrawTexturedRect(ScrW()*0.5 - 128, ScrH()*0.5 - 120.5, 256, 241)
-    end)
-  end
+IncredibleAPI:Call("SteamAvatar", ply, "!!! Your SteamAPI-Key Here !!!", function(avatar_url) -- Get player avatar url
+	print(avatar_url)
 end)
+
+IncredibleAPI:Call("SteamBans", ply, "!!! Your SteamAPI-Key Here !!!", function(bans_data) -- Get player bans
+	PrintTable(bans_data)
+end)
+
