be38f5775b4f5ee148bacc59f26dd10c6667e899
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-06-14 21:44:33 +0700

    Update sv_geoip.lua

--- garrysmod/lua/incredible_api/sv_geoip.lua
+++ garrysmod/lua/incredible_api/sv_geoip.lua
@@ -6,15 +6,13 @@
         Discord: discord.incredible-gmod.ru
 --——————————————————————————————————————————————]]--
 
-local Fetch, JSONToTable, IsValid, isstr = http.Fetch, util.JSONToTable, IsValid, isstring
+local isstr = isstring
 
 local APIModule = {}
 APIModule.Name = "GeoIP"
 APIModule.ApiURL = "https://freegeoip.app/json/%s" -- http://ip-api.com/json/%s?fields=countryCode  (Another GeoAPI)
 function APIModule:Call(target, callback)
-	if not isstr(target) and IsValid(target) and target:IsPlayer() then
-		target = target:IPAddress()
-	end
+	target = self:RequestSteamID64(target)
 	if not isstr(target) then return end
 
 	local cache = self:GetCache(target)
@@ -23,15 +21,14 @@ function APIModule:Call(target, callback)
 		return
 	end
 
-	print( self.ApiURL:format(target) )
 	self:FetchURL(self.ApiURL:format(target), function(body)
 		if not body or body == "" then return end
-		local tbl = JSONToTable(body)
-		if not tbl or not tbl["country_code"] then return end
+		local result = self:HandleJson(body, "country_code")
+		if not result then return end
 
-		self:DoCache(target, tbl["country_code"])
+		self:DoCache(target, result)
 		if callback then
-			callback(tbl["country_code"])
+			callback(result)
 		end
 	end)
 end
