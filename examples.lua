b9633484e83dbef37928126a87f5022e0027143a
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-06-14 22:49:30 +0700

    Update examples.lua

--- examples.lua
+++ examples.lua
@@ -21,20 +21,25 @@ IncredibleAPI:Call("Proxy", ply, function(is_use_proxy) -- Anti-proxy API
 	ply:Kick("Proxy is not allowed!")
 end)
 
-IncredibleAPI:Call("SteamPlaytime", ply, "!!! Your SteamAPI-Key Here !!!", function(seconds) -- Get gmod playtime
+local steam_api_key = "!!! Your SteamAPI-Key Here !!!"
+IncredibleAPI:Call("SteamPlaytime", ply, steam_api_key, function(seconds) -- Get gmod playtime
 	print(seconds/60)
 end)
 
-IncredibleAPI:Call("SteamAvatar", ply, "!!! Your SteamAPI-Key Here !!!", function(avatar_url) -- Get player avatar url
+IncredibleAPI:Call("SteamAvatar", ply, steam_api_key, function(avatar_url) -- Get player avatar url
 	print(avatar_url)
 end)
 
-IncredibleAPI:Call("SteamBans", ply, "!!! Your SteamAPI-Key Here !!!", function(bans_data) -- Get player bans
+IncredibleAPI:Call("SteamBans", ply, steam_api_key, function(bans_data) -- Get player bans
 	PrintTable(bans_data)
 end)
 
-IncredibleAPI:Call("SteamFamilyShared", ply, "!!! Your SteamAPI-Key Here !!!", function(is_family_shared) -- Anti-FamilyShared
+IncredibleAPI:Call("SteamFamilyShared", ply, steam_api_key, function(is_family_shared) -- Anti-FamilyShared
 	if is_family_shared then
 		ply:Kick("Family-Shared is not allowed!")
 	end
+end)
+
+IncredibleAPI:Call("SteamIsInGroup", ply, steam_api_key, "4822177", function(bool) -- Check if user member of group
+	print(bool)
 end)
